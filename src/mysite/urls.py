2a8d78aea82d25a876cd78141ebb1e5804b9ebb3
oladimeji <tommyslipz@gmail.com>
2019-08-08 09:30:38 +0100

    commit

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     path('contactus/', contactus_view, name='contactus'),
     path('account/', account_view, name='account'),
     path('cars/', include('cars.urls','cars')),
-    path('home/', home_screen_view, name='home'),
+    path('', home_screen_view, name='home'),
     path('register/',registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
     path('login/', login_view, name='login'),
